75eebcdf9e2600b01edb30bd341e0239e710789a
somename <someemail>
2015-10-14 14:51:48 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/Frame.java
+++ src/org/unioulu/tol/sqat/bsc/Frame.java
@@ -9,6 +9,14 @@ public class Frame {
 		this.secondThrow = secondThrow;
 	}
 	
+	public int getFirstThrow() {
+		return firstThrow;
+	}
+
+	public int getSecondThrow() {
+		return secondThrow;
+	}
+
 	//the score of a single frame
 	public int score(){
 		//to be implemented
